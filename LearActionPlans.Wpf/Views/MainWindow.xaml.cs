b2326c6336b0d87d1c749502998fc0080b403f96
Marek Lovčí <mareklovci@gmail.com>
2021-07-24 12:56:09 +0200

    Simple preview App

---
+++ LearActionPlans.Wpf/Views/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace LearActionPlans.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick1(object sender, RoutedEventArgs e)
+        {
+            var win2 = new NewActionPlanView();
+            win2.Show();
+        }
+    }
+}
---
+++ LearActionPlans.Wpf/Views/NewActionPlanView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using LearActionPlans.Wpf.Models;
+
+namespace LearActionPlans.Wpf.Views
+{
+    public partial class NewActionPlanView : Window
+    {
+        public NewActionPlanView()
+        {
+            InitializeComponent();
+            
+            var id = Guid.NewGuid();
+
+            // Populate list
+            using (var context = new LearDataAllEntities())
+            {
+                var query = from ap in context.AkcniPlany select ap; 
+
+                foreach (var ap in query)
+                {
+                    listView.Items.Add(ap);
+                }
+            }
+        }
+    }
+}

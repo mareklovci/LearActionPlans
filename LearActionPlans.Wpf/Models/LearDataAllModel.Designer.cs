49ed705efcfc7d15e4a2e59a780c150bbcac37a4
romanbartos <59966990+romanbartos@users.noreply.github.com>
2021-07-25 20:03:58 +0200

    Lower .NET and Connection String Change

--- LearActionPlans.Wpf/Models/LearDataAllModel.Designer.cs
+++ LearActionPlans.Wpf/Models/LearDataAllModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\marek\gitrepos\LearActionPlans\LearActionPlans.Wpf\Models\LearDataAllModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\pc\source\repos\LearActionPlans\LearActionPlans.Wpf\Models\LearDataAllModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
